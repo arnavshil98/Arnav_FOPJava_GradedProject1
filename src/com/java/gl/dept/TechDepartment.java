68705ab53a45e0cf5215d4fe17880d8edb7a413b
arnavshil98 <arnv.shil@connaissant.com>
2022-10-09 16:46:39 +0530

    main portal created

--- src/com/java/gl/dept/TechDepartment.java
+++ src/com/java/gl/dept/TechDepartment.java
@@ -1,26 +1,24 @@
 package com.java.gl.dept;
 
-public class HrDepartment extends SuperDepartment {
+public class TechDepartment extends SuperDepartment {
 
 	@Override
 	public String departmentName() {
-		return "Welcome to Hr Department";
+		return "Tech Department";
 	}
 
 	@Override
 	public String getTodaysWork() {
-		return "Fill Todays timesheet and mark your attendance";
+		return "Complete coding of module 1";
 	}
 
 	@Override
 	public String getWorkDeadline() {
-		return "Complete by EOD";
-
+		return "Complete byEOD";
 	}
 
-	public String doActivity() {
-		return "team Lunch";
-
+	public String getTechStackInformation() {
+		return "core Java";
 	}
 
 	public String isTodayAHoliday() {
